1ad5a7876ec1dc774374e97e925b6949c7920b09
Leonardo Vasconcelos <leobav1988@gmail.com>
2026-05-31 19:17:31 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+
+    return redirect('/admin');
+    //return view('welcome');
+    //redirect('/admin');
+    //return view('welcome');
+});
