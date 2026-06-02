e8fb3bda5f848fc5bc3b41161703118077dac7b6
Leonardo Vasconcelos <leobav1988@gmail.com>
2026-06-01 23:53:10 -0300

    fds

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,17 @@ use App\Http\Controllers\Api\MovementController;
 use App\Services\LocalNetworkService;
 use Illuminate\Support\Facades\Route;
 
-Route::get('healthy', fn () => response()->json(['status' => 'ok']))
-    ->name('healthy');
-
-Route::get('local-network', function (LocalNetworkService $localNetworkService) {
+Route::get('healthy', function (LocalNetworkService $localNetworkService) {
     $baseUrl = $localNetworkService->baseUrl();
 
     return response()->json([
+        'status' => 'ok',
         'ip' => $localNetworkService->localIp(),
         'port' => $localNetworkService->port(),
         'url' => $baseUrl,
         'healthy_url' => $baseUrl ? "{$baseUrl}/api/healthy" : null,
     ]);
-})->name('local-network');
+})->name('healthy');
 
 Route::post('movements', MovementController::class)
     ->name('movements.store');
